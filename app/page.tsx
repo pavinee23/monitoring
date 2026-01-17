501e864d9e45f57bf96f42b63526b9995a595b54
K Energy Save <k-system@kenergysave.com>
2026-01-17 12:30:45 +0900

    fix: use Next.js server-side redirect to main.html

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-'use client'
-
-import { useEffect } from 'react'
+import { redirect } from 'next/navigation'
 
 export default function Page() {
-  useEffect(() => {
-    window.location.replace('/main.html')
-  }, [])
-
-  return null
+  redirect('/main.html')
 }
 
 
